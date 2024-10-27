4800fc9213eda45dc0abed4591d7f5932c97e000
Abinesha312 <Abinesha312@gmail.com>
2024-10-27 11:55:25 -0500

    Button changes

--- frontend/src/landingpage.js
+++ frontend/src/landingpage.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Logo from "./assets/logo.jpg"; 
 import MockupImage from "./assets/mockup.jpg"; 
 import "./landingpage.css";
 
 const LandingPage = () => {
+    const navigate = useNavigate();
+
+    const handleButtonClick = () => {
+        navigate('/upload');
+    };
+
     return (
       <div className="landing-page">
         <header className="header">
@@ -14,14 +21,14 @@ const LandingPage = () => {
         <div className="text-content">
           <h2>About Us</h2>
           <p>Got transactions? Let's give them a quick once-over for any sneaky fraudsters!</p>
-          <button className="cta-button">Go ahead, and try it out</button>
-      </div>
+          <button className="cta-button" onClick={handleButtonClick}>Go ahead, and try it out</button>
+        </div>
 
-      <div className="image-content">
-        <img src={MockupImage} alt="Credit Card Mockup" className="mockup-image" />
+        <div className="image-content">
+          <img src={MockupImage} alt="Credit Card Mockup" className="mockup-image" />
+        </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default LandingPage;
